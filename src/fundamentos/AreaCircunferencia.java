6678fce86a0c85aa576e3d883320c4968de78e33
JohnMaykon <johnmikon@gmail.com>
2024-07-08 15:41:35 -0300

    Primeiras aulas

---
+++ src/fundamentos/AreaCircunferencia.java
@@ -0,0 +1,18 @@
+package fundamentos;
+
+@SuppressWarnings("ALL")
+public class AreaCircunferencia {
+    public static void main(String[] args) {
+        double raio = 3.4;
+            final double PI = 3.14159;
+        /* final significa que a variavel é uma constante;
+        Por isso PI está em maiuscula pois existe uma convenção de boas maneiras que toda constante deve ser escrita em letras MAIUSCULAS;
+        */
+        double area = PI * raio * raio;
+        System.out.println(area);
+
+        raio = 10;
+        area = PI * raio * raio;
+        System.out.println("Área = " + area + "m².");
+    }
+}
---
+++ src/fundamentos/PrimeiroPrograma.java
@@ -0,0 +1,27 @@
+package fundamentos;
+/**
+ * Esse comentario representa o java DOC...
+ *
+ @author johnm
+ @since JDK 21
+ @see
+*/
+@SuppressWarnings("ALL")
+public class PrimeiroPrograma {
+    /**
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+        System.out.println("Primeiro programa");
+        //Uma sentença de código termina com ;
+        //Mais um comentáro...
+        //Fim
+        System.out.println("Segundo programa");
+        /*
+        *Linha 1
+        *Linha 2
+        *Linha 3...
+        */
+    }
+}
